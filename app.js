03423d343a6fe296fb893b27d2987721cae9d31a
Waasim Ansari <161191967+waasimAnsari786@users.noreply.github.com>
2024-07-31 19:14:20 +0500

    I changed the UI and make it responsive.

--- app.js
+++ app.js
@@ -2,64 +2,109 @@ let homeForm = document.querySelector(".home-f");
 let graphCtnr = document.querySelector(".graph-ctnr");
 let graphCtnr2 = document.querySelector(".graph-ctnr-2");
 
+// this function is for navigate new page
 const navigateToNextPageFunc = (page) => {
   window.location.href = page;
 };
 
+
+// remove the class function
 const addClassFunc = (elem, className) => {
   elem.classList.add(className);
 };
 
+// add the class function
 const remClassFunc = (elem, className) => {
   elem.classList.remove(className);
 };
 
+
+// these 2 funtions "addAniFunc" and "remAniFunc" are for toggling the sub buttons div on dashboard
+const addAniFunc = (targElem, myHeight , num) => {
+  requestAnimationFrame(() => {
+    targElem.style.height = myHeight;
+    targElem.style.marginBottom = "2.5rem";
+    if (num === 0) {
+      setTimeout(() => {
+        targElem.style.opacity = "1";
+      }, 600);
+    } else {
+      targElem.style.opacity = "1";
+    }
+  });
+};
+const remAniFunc = (targElem) => {
+  targElem.style.height = "0rem";
+  targElem.style.opacity = "0";
+  targElem.style.marginBottom = "0";
+  targElem.innerHTML = "";
+};
+
+
+// this function is for showing and hidding each form of every sub button
+const showHideInpCtnrFunc = (targElem) => {
+  if (targElem) {
+    if (num2 === 0) {
+      remClassFunc(targElem, "hide");
+      num2 = 1;
+    } else {
+      addClassFunc(targElem, "hide");
+      num2 = 0;
+    }
+  }
+};
+
+
+// this is "Chart.Js" library's code for inserting charts
 if (graphCtnr) {
-  const ctx = document.getElementById('barchart');
-  
-    new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-          label: '# of Votes',
+  const ctx = document.getElementById("barchart");
+  new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      datasets: [
+        {
+          label: "# of Votes",
           data: [12, 19, 3, 5, 2, 3],
-          borderWidth: 1
-        }]
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
       },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true
-          }
-        }
-      }
-    });
+    },
+  });
 }
-
 if (graphCtnr2) {
-  const ctx = document.getElementById('doughnut');
-  
-    new Chart(ctx, {
-      type: 'doughnut',
-      data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-          label: '# of Votes',
+  const ctx = document.getElementById("doughnut");
+  new Chart(ctx, {
+    type: "doughnut",
+    data: {
+      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      datasets: [
+        {
+          label: "# of Votes",
           data: [12, 19, 3, 5, 2, 3],
-          borderWidth: 1
-        }]
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
       },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true
-          }
-        }
-      }
-    });
+    },
+  });
 }
 
+
+// this is for prevnting the submit functionality of forms
 if (homeForm) {
   homeForm.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -78,73 +123,79 @@ let num = 0;
 let num2 = 0;
 let subBtnCtnr;
 
-let inpCtnr = document.querySelector(".inp-ctnr");
 
+
+// all the sub buttons and their forms targeted in this function.
 optCtnr.addEventListener("click", (e) => {
-  if (e.target.innerText === "Vendor") {
+  if (
+    e.target.id === "vendor-btn" ||
+    e.target.innerText === "Vendor" ||
+    e.target.id === "vendor-icon"
+  ) {
     if (!subBtnCtnr) {
       subBtnCtnr = document.createElement("div");
+      addClassFunc(subBtnCtnr, "sub-opt-ctnr");
     }
 
     if (num === 0) {
-      addClassFunc(subBtnCtnr, "col-8");
-      addClassFunc(subBtnCtnr, "sub-opt-ctnr");
       subBtnCtnr.innerHTML = `<button class="w-100 sub-opts">add ${e.target.innerText}</button>
                             <button class="w-100 sub-opts">add product</button>
-                            <button class="w-100 sub-opts">transaction</button>
-                            <button class="w-100 sub-opts">ledger</button>`;
-      e.target.parentElement.append(subBtnCtnr);
+                            <button class="w-100 sub-opts">${e.target.innerText}'s transaction</button>
+                            <button class="w-100 sub-opts">${e.target.innerText}'s ledger</button>`;
+      e.target.closest(".opt-ctnr-inner").append(subBtnCtnr);
+      addAniFunc(subBtnCtnr , '20rem' , 0)
       num = 1;
     } else {
-      subBtnCtnr.innerHTML = "";
+      remAniFunc(subBtnCtnr)
       num = 0;
     }
   } else if (e.target.classList.contains("sub-opts")) {
-    if (dbFormCtnr) {
-      if (num2 === 0) {
-        remClassFunc(dbFormCtnr, "hide");
-        num2 = 1;
-      } else {
-        addClassFunc(dbFormCtnr, "hide");
-        num2 = 0;
-      }
-    }
+    showHideInpCtnrFunc(dbFormCtnr)
   }
 
-  if (e.target.innerText === "Client") {
+  if (
+    e.target.id === "client-btn" ||
+    e.target.innerText === "Client" ||
+    e.target.id === "client-icon"
+  ) {
     if (!subBtnCtnr) {
       subBtnCtnr = document.createElement("div");
     }
 
     if (num === 0) {
-      addClassFunc(subBtnCtnr, "col-8");
       addClassFunc(subBtnCtnr, "sub-opt-ctnr");
       subBtnCtnr.innerHTML = `<button class="w-100 sub-opts">add ${e.target.innerText}</button>
-                            <button class="w-100 sub-opts">${e.target.innerText}'s entry</button>`;
-      e.target.parentElement.append(subBtnCtnr);
+                            <button class="w-100 sub-opts">${e.target.innerText}'s entry</button>
+                            <button class="w-100 sub-opts">${e.target.innerText}'s ledger</button>`;
+      e.target.closest(".opt-ctnr-inner").append(subBtnCtnr);
+      addAniFunc(subBtnCtnr , '15rem' , 0)
       num = 1;
     } else {
-      subBtnCtnr.innerHTML = "";
+      remAniFunc(subBtnCtnr)
       num = 0;
     }
   }
 
-  if (e.target.innerText === "Payroll") {
+  if (
+    e.target.id === "payroll-btn" ||
+    e.target.innerText === "Payroll" ||
+    e.target.id === "payroll-icon"
+  ) {
     if (!subBtnCtnr) {
       subBtnCtnr = document.createElement("div");
     }
 
     if (num === 0) {
-      addClassFunc(subBtnCtnr, "col-8");
       addClassFunc(subBtnCtnr, "sub-opt-ctnr");
       subBtnCtnr.innerHTML = `<button class="w-100 sub-opts">add employee</button>
                             <button class="w-100 sub-opts">advance salary</button>
                             <button class="w-100 sub-opts">${e.target.innerText} entry</button>
                             <button class="w-100 sub-opts">${e.target.innerText} ledger</button>`;
-      e.target.parentElement.append(subBtnCtnr);
+      e.target.closest(".opt-ctnr-inner").append(subBtnCtnr);
+      addAniFunc(subBtnCtnr , '20rem' , 1)
       num = 1;
     } else {
-      subBtnCtnr.innerHTML = "";
+      remAniFunc(subBtnCtnr)
       num = 0;
     }
   }
