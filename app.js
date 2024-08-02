f6b8921c75e0855d5727c337a4b9c1313d1f6b05
Waasim Ansari <161191967+waasimAnsari786@users.noreply.github.com>
2024-08-02 19:17:24 +0500

    I printed all the forms according to every button through JavaScript.

--- app.js
+++ app.js
@@ -1,12 +1,75 @@
+// import forms data API for printing the forms
+import { formsDataAPI } from "./formAPI.js";
+
+// login page's code
 let homeForm = document.querySelector(".home-f");
-let graphCtnr = document.querySelector(".graph-ctnr");
-let graphCtnr2 = document.querySelector(".graph-ctnr-2");
+let emailInp = document.querySelector("#email-inp");
+let pswdInp = document.querySelector("#pswd-inp");
 
 // this function is for navigate new page
 const navigateToNextPageFunc = (page) => {
   window.location.href = page;
 };
 
+// this function is for validating user's Email
+const validateEmailFunc = (userEmail) => {
+  return /^[\da-zA-Z]+(?:[+%._-][\da-zA-Z]+)*@[a-zA-Z\d]+(?:[-])*\.[A-Za-z]{2,}$/.test(
+    userEmail
+  );
+};
+
+// this function is for validating user's password
+const validatePswdfunc = (userPswd) => {
+  return /^((?=.*[a-z]+)(?=.*[A-Z]+)(?=.*[\d]+)(?=.*[\W_]+)).{8,}$/.test(
+    userPswd
+  );
+};
+
+// this function if for subit button of login page
+const chkEmailPswdOnClick = (CBEmaliFunc, CBPswdFunc, a, b) => {
+  let checkedEmail = CBEmaliFunc(a);
+  let checkedPswd = CBPswdFunc(b);
+  return [checkedEmail, checkedPswd];
+};
+
+// this is for prevnting the submit functionality of forms
+if (homeForm) {
+  homeForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
+
+  // this event listener is for targeting the submit button of login page and then check user's email and password through the function which are defined at above
+  homeForm.addEventListener("click", (e) => {
+    if (e.target.id === "submit-btn") {
+      // i used callback hell in this line of code
+      let [finalValue1, finalValue2] = chkEmailPswdOnClick(
+        validateEmailFunc,
+        validatePswdfunc,
+        emailInp.value,
+        pswdInp.value
+      );
+
+      // these are the conditions for alerting the user that what mistake is he doing at the time of login
+      if (finalValue1 && finalValue2) {
+        navigateToNextPageFunc("dashbord.html");
+      } else if (!finalValue1 && !finalValue2) {
+        alert("Both email and password are incorrect.");
+        navigateToNextPageFunc("index.html");
+      } else if (!finalValue1) {
+        alert("Your email is incorrect.");
+        navigateToNextPageFunc("index.html");
+      } else if (!finalValue2) {
+        alert("Your password is incorrect.");
+        navigateToNextPageFunc("index.html");
+      }
+    }
+  });
+}
+// login page's code end
+
+// dashboard start
+let graphCtnr = document.querySelector(".graph-ctnr");
+let graphCtnr2 = document.querySelector(".graph-ctnr-2");
 
 // remove the class function
 const addClassFunc = (elem, className) => {
@@ -18,9 +81,8 @@ const remClassFunc = (elem, className) => {
   elem.classList.remove(className);
 };
 
-
 // these 2 funtions "addAniFunc" and "remAniFunc" are for toggling the sub buttons div on dashboard
-const addAniFunc = (targElem, myHeight , num) => {
+const addAniFunc = (targElem, myHeight, num) => {
   requestAnimationFrame(() => {
     targElem.style.height = myHeight;
     targElem.style.marginBottom = "2.5rem";
@@ -40,20 +102,13 @@ const remAniFunc = (targElem) => {
   targElem.innerHTML = "";
 };
 
-
-// this function is for showing and hidding each form of every sub button
-const showHideInpCtnrFunc = (targElem) => {
-  if (targElem) {
-    if (num2 === 0) {
-      remClassFunc(targElem, "hide");
-      num2 = 1;
-    } else {
-      addClassFunc(targElem, "hide");
-      num2 = 0;
-    }
-  }
+const printForms = (targElem , plchldr) => {
+  let inp = document.createElement("input");
+  inp.setAttribute("type", "text");
+  inp.setAttribute("placeholder", `${plchldr}`);
+  inp.classList.add("form-inps");
+  targElem.append(inp);
 };
-
 
 // this is "Chart.Js" library's code for inserting charts
 if (graphCtnr) {
@@ -103,100 +158,164 @@ if (graphCtnr2) {
   });
 }
 
-
-// this is for prevnting the submit functionality of forms
-if (homeForm) {
-  homeForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-  });
-
-  homeForm.addEventListener("click", (e) => {
-    if (e.target.classList.contains("sub-btn")) {
-      navigateToNextPageFunc("dashbord.html");
-    }
-  });
-}
-
 let optCtnr = document.querySelector(".opt-ctnr");
-let dbFormCtnr = document.querySelector(".inp-ctnr");
+let dbFormCtnr = document.querySelector(".inp-ctnr form");
 let num = 0;
-let num2 = 0;
 let subBtnCtnr;
 
-
-
 // all the sub buttons and their forms targeted in this function.
-optCtnr.addEventListener("click", (e) => {
-  if (
-    e.target.id === "vendor-btn" ||
-    e.target.innerText === "Vendor" ||
-    e.target.id === "vendor-icon"
-  ) {
-    if (!subBtnCtnr) {
-      subBtnCtnr = document.createElement("div");
-      addClassFunc(subBtnCtnr, "sub-opt-ctnr");
-    }
+if (optCtnr) {
+  optCtnr.addEventListener("click", (e) => {
+    if (
+      e.target.id === "vendor-btn" ||
+      e.target.innerText === "Vendor" ||
+      e.target.id === "vendor-icon"
+    ) {
+      if (!subBtnCtnr) {
+        subBtnCtnr = document.createElement("div");
+        addClassFunc(subBtnCtnr, "sub-opt-ctnr");
+      }
 
-    if (num === 0) {
-      subBtnCtnr.innerHTML = `<button class="w-100 sub-opts">add ${e.target.innerText}</button>
+      if (num === 0) {
+        subBtnCtnr.innerHTML = `<button class="w-100 sub-opts">add ${e.target.innerText}</button>
                             <button class="w-100 sub-opts">add product</button>
                             <button class="w-100 sub-opts">${e.target.innerText}'s transaction</button>
                             <button class="w-100 sub-opts">${e.target.innerText}'s ledger</button>`;
-      e.target.closest(".opt-ctnr-inner").append(subBtnCtnr);
-      addAniFunc(subBtnCtnr , '20rem' , 0)
-      num = 1;
-    } else {
-      remAniFunc(subBtnCtnr)
-      num = 0;
-    }
-  } else if (e.target.classList.contains("sub-opts")) {
-    showHideInpCtnrFunc(dbFormCtnr)
-  }
-
-  if (
-    e.target.id === "client-btn" ||
-    e.target.innerText === "Client" ||
-    e.target.id === "client-icon"
-  ) {
-    if (!subBtnCtnr) {
-      subBtnCtnr = document.createElement("div");
+        e.target.closest(".opt-ctnr-inner").append(subBtnCtnr);
+        addAniFunc(subBtnCtnr, "20rem", 0);
+        num = 1;
+      } else {
+        remAniFunc(subBtnCtnr);
+        num = 0;
+      }
     }
 
-    if (num === 0) {
-      addClassFunc(subBtnCtnr, "sub-opt-ctnr");
-      subBtnCtnr.innerHTML = `<button class="w-100 sub-opts">add ${e.target.innerText}</button>
+    if (
+      e.target.id === "client-btn" ||
+      e.target.innerText === "Client" ||
+      e.target.id === "client-icon"
+    ) {
+      if (!subBtnCtnr) {
+        subBtnCtnr = document.createElement("div");
+      }
+
+      if (num === 0) {
+        addClassFunc(subBtnCtnr, "sub-opt-ctnr");
+        subBtnCtnr.innerHTML = `<button class="w-100 sub-opts">add ${e.target.innerText}</button>
                             <button class="w-100 sub-opts">${e.target.innerText}'s entry</button>
                             <button class="w-100 sub-opts">${e.target.innerText}'s ledger</button>`;
-      e.target.closest(".opt-ctnr-inner").append(subBtnCtnr);
-      addAniFunc(subBtnCtnr , '15rem' , 0)
-      num = 1;
-    } else {
-      remAniFunc(subBtnCtnr)
-      num = 0;
-    }
-  }
-
-  if (
-    e.target.id === "payroll-btn" ||
-    e.target.innerText === "Payroll" ||
-    e.target.id === "payroll-icon"
-  ) {
-    if (!subBtnCtnr) {
-      subBtnCtnr = document.createElement("div");
+        e.target.closest(".opt-ctnr-inner").append(subBtnCtnr);
+        addAniFunc(subBtnCtnr, "15rem", 0);
+        num = 1;
+      } else {
+        remAniFunc(subBtnCtnr);
+        num = 0;
+      }
     }
 
-    if (num === 0) {
-      addClassFunc(subBtnCtnr, "sub-opt-ctnr");
-      subBtnCtnr.innerHTML = `<button class="w-100 sub-opts">add employee</button>
+    if (
+      e.target.id === "payroll-btn" ||
+      e.target.innerText === "Payroll" ||
+      e.target.id === "payroll-icon"
+    ) {
+      if (!subBtnCtnr) {
+        subBtnCtnr = document.createElement("div");
+      }
+
+      if (num === 0) {
+        addClassFunc(subBtnCtnr, "sub-opt-ctnr");
+        subBtnCtnr.innerHTML = `<button class="w-100 sub-opts">add employee</button>
                             <button class="w-100 sub-opts">advance salary</button>
                             <button class="w-100 sub-opts">${e.target.innerText} entry</button>
                             <button class="w-100 sub-opts">${e.target.innerText} ledger</button>`;
-      e.target.closest(".opt-ctnr-inner").append(subBtnCtnr);
-      addAniFunc(subBtnCtnr , '20rem' , 1)
-      num = 1;
-    } else {
-      remAniFunc(subBtnCtnr)
-      num = 0;
+        e.target.closest(".opt-ctnr-inner").append(subBtnCtnr);
+        addAniFunc(subBtnCtnr, "20rem", 1);
+        num = 1;
+      } else {
+        remAniFunc(subBtnCtnr);
+        num = 0;
+      }
+    } else if (e.target.innerText === "Add Vendor") {
+      formsDataAPI.forEach((element) => {
+        if (element.hasOwnProperty("Add Vendor")) {
+          dbFormCtnr.innerHTML = "";
+          element["Add Vendor"].forEach((innElem) => {
+            printForms(dbFormCtnr , innElem)
+          });
+        }
+      });
+      dbFormCtnr.previousElementSibling.innerText = `${e.target.innerText}'s form`;
+    } else if (e.target.innerText === "Add Product") {
+      formsDataAPI.forEach((element) => {
+        if (element.hasOwnProperty("Add Product")) {
+          dbFormCtnr.innerHTML = "";
+          element["Add Product"].forEach((innElem) => {
+            printForms(dbFormCtnr , innElem)
+          });
+        }
+      });
+      dbFormCtnr.previousElementSibling.innerText = `${e.target.innerText}'s form`;
     }
-  }
-});
+
+    else if (e.target.innerText === "Add Client") {
+      formsDataAPI.forEach((element) => {
+        if (element.hasOwnProperty("Add Client")) {
+          dbFormCtnr.innerHTML = "";
+          element["Add Client"].forEach((innElem) => {
+            printForms(dbFormCtnr , innElem)
+          });
+        }
+      });
+      dbFormCtnr.previousElementSibling.innerText = `${e.target.innerText}'s form`;
+    }
+
+    else if (e.target.innerText === "Client's Entry") {
+      formsDataAPI.forEach((element) => {
+        if (element.hasOwnProperty("Client's Entry")) {
+          dbFormCtnr.innerHTML = "";
+          element["Client's Entry"].forEach((innElem) => {
+            printForms(dbFormCtnr , innElem)
+          });
+        }
+      });
+      dbFormCtnr.previousElementSibling.innerText = `${e.target.innerText}'s form`;
+    }
+
+    else if (e.target.innerText === "Add Employee") {
+      formsDataAPI.forEach((element) => {
+        if (element.hasOwnProperty("Add Employee")) {
+          dbFormCtnr.innerHTML = "";
+          element["Add Employee"].forEach((innElem) => {
+            printForms(dbFormCtnr , innElem)
+          });
+        }
+      });
+      dbFormCtnr.previousElementSibling.innerText = `${e.target.innerText}'s form`;
+    }
+
+    else if (e.target.innerText === "Advance Salary") {
+      formsDataAPI.forEach((element) => {
+        if (element.hasOwnProperty("Advance Salary")) {
+          dbFormCtnr.innerHTML = "";
+          element["Advance Salary"].forEach((innElem) => {
+            printForms(dbFormCtnr , innElem)
+          });
+        }
+      });
+      dbFormCtnr.previousElementSibling.innerText = `employee's ${e.target.innerText}'s form`;
+    }
+
+    else if (e.target.innerText === "Payroll Entry") {
+      formsDataAPI.forEach((element) => {
+        if (element.hasOwnProperty("Payroll Entry")) {
+          dbFormCtnr.innerHTML = "";
+          element["Payroll Entry"].forEach((innElem) => {
+            printForms(dbFormCtnr , innElem)
+          });
+        }
+      });
+      dbFormCtnr.previousElementSibling.innerText = `${e.target.innerText}'s form`;
+    }
+  });
+}
+// dashboard end
---
+++ formAPI.js
@@ -0,0 +1,106 @@
+let formsDataAPI = [
+  {
+    "Add Vendor": [
+      "s.no",
+      "date",
+      "vendor name",
+      "address",
+      "cell num",
+      "product",
+      "bags",
+      "bags weight",
+      "price",
+      "total value",
+      "advance payment",
+      "amount payable",
+      "outstanding payable",
+      "balance",
+      "edit",
+      "delete",
+    ],
+  },
+
+  {
+    "Add Product": [
+      "s.no",
+      "date",
+      "product name",
+      "product size",
+      "num of bundlles",
+      "KGs",
+      "price",
+    ],
+  },
+
+  {
+    "Add Client": [
+      "s.no",
+      "date",
+      "client name",
+      "address",
+      "client's cell num",
+    ],
+  },
+
+  {
+    "Client's Entry": [
+      "s.no",
+      "date",
+      "client name",
+      "address size",
+      "client's cell num",
+      "product",
+      "size",
+      "bundles",
+      "FT",
+      "price",
+      "total value",
+      "amount receivable",
+      "received by client",
+      "outstanding amount",
+      "edit",
+      "delete",
+    ],
+  },
+
+  {
+    "Add Employee": [
+      "s.no",
+      "date",
+      "employee name",
+      "designation",
+      "emp's cell nums",
+      "emp's salary"
+    ],
+  },
+
+  {
+    "Advance Salary": [
+      "s.no",
+      "date",
+      "employee name",
+      "designation",
+      "emp's cell nums",
+      "emp's salary"
+    ],
+  },
+
+  {
+    "Payroll Entry": [
+      "s.no",
+      "date",
+      "employee name",
+      "designation",
+      "month working days",
+      "emp working days",
+      "leaves",
+      "salary",
+      "advance",
+      "net salary",
+      "edit",
+      "delete",
+    ],
+  },
+];
+
+export { formsDataAPI };
